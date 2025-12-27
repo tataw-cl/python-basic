4eac0ff1f3eddbe3ca4dd136440c993e2f707b4b
akenjifaith1@gmail.com <127490325+tataw-cl@users.noreply.github.com>
2025-12-27 22:30:26 +0100

    Functions

--- Hello.py
+++ Hello.py
@@ -1,1 +1,8 @@
 print("Hello world")
+#Using some mathematical functions
+import math
+print("The square root of 16 is:", math.sqrt(16))
+print("The value of pi is:", math.pi)
+print("The cosine of 0 is:", math.cos(0))
+print("The sine of 90 degrees is:", math.sin(math.radians(90)))
+print("The logarithm of 1000 is:", math.log10(1000))
