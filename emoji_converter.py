fedc401dab9da81185deb4043e77d24b51aa12e5
akenjifaith1@gmail.com <127490325+tataw-cl@users.noreply.github.com>
2026-01-04 23:44:40 +0100

    Functions in python

--- emoji_converter.py
+++ emoji_converter.py
@@ -1,27 +1,29 @@
-emojiDict = {":)": "😊",
-             ":(": "😞",
-             ":D": "😃",
-             ";)": "😉",
-             ":P": "😛",
-             ":'(": "😢",
-             ":O": "😮",
-             ":/": "😕",
-             ":|": "😐",
-             "<3": "❤️",
-             ":*": "😘",
-             "XD": "😆",
-             "B)": "😎",
-             "-_-": "😑",
-             "^_^": "😊",
-             ">:(": "😠",
-             ":3": "😺",
-             ":$": "😳",
-             ":^)": "🙂"
-             }
-message = input("> ").split()
-for word in range(len(message)):
-    if message[word] in emojiDict:
-        message[word] = emojiDict[message[word]]
-res = ""
-res += " ".join(message)
-print(res)
+def emoji_converter(message):
+    emojiDict = {":)": "😊",
+                ":(": "😞",
+                ":D": "😃",
+                ";)": "😉",
+                ":P": "😛",
+                ":'(": "😢",
+                ":O": "😮",
+                ":/": "😕",
+                ":|": "😐",
+                "<3": "❤️",
+                ":*": "😘",
+                "XD": "😆",
+                "B)": "😎",
+                "-_-": "😑",
+                "^_^": "😊",
+                ">:(": "😠",
+                ":3": "😺",
+                ":$": "😳",
+                ":^)": "🙂"
+                }
+    res = ""
+    for word in message:
+        res += emojiDict.get(word, word) + " "
+    return res
+
+
+msg = input("> ").split()
+print(emoji_converter(msg))
